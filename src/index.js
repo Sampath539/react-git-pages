074ed532dcb4fc8a11c379056791b55324f4afce
Gannamaneni Sampath Kumar <Sampath.Gannamaneni@ibm.com>
2022-01-06 23:00:24 +0530

    scr

--- src/index.js
+++ src/index.js
@@ -3,21 +3,19 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { HashRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 import Login from './Login';
 import Home from './Home';
 
 ReactDOM.render(
   <React.StrictMode>
     {console.log(process.env.PUBLIC_URL)}
-    <HashRouter>
+    <BrowserRouter basename='/react-git-pages'>
       {/* <App /> */}
-      <Routes>
-        <Route path={process.env.PUBLIC_URL + '/'} element={<App />} />
-        <Route path={process.env.PUBLIC_URL + '/login'} element={<Login />} />
-        <Route path={process.env.PUBLIC_URL + '/home'} element={<Home />} />
-      </Routes>
-    </HashRouter>
+      <Route path='/' element={<App />} />
+      <Route path='/login' element={<Login />} />
+      <Route path='/home' element={<Home />} />
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
